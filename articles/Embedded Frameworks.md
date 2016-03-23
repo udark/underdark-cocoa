f847a1bd0c0e9b24346dfe357b457a3c71e7e90e
Virl <virlof@gmail.com>
2016-03-23 18:40:52 +0300

    Build fixes.

---
+++ articles/Embedded Frameworks.md
@@ -0,0 +1,5 @@
+Embedding a framework within a framework:
+
+http://stackoverflow.com/questions/27868259/embedding-a-framework-within-a-framework-ios-8
+
+http://stackoverflow.com/questions/7365578/why-are-umbrella-frameworks-discouraged
--- underdark/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ underdark/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -82,3 +82,12 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods/MSWeakTimer.framework"
+  install_framework "Pods/ProtocolBuffers.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods/MSWeakTimer.framework"
+  install_framework "Pods/ProtocolBuffers.framework"
+fi
