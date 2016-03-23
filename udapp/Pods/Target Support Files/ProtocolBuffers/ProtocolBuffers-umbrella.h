901755dc96f53aa37a354935aa4a31216aeb6114
Virl <virlof@gmail.com>
2016-03-23 18:43:17 +0300

    More build fixes.

---
+++ udapp/Pods/Target Support Files/ProtocolBuffers/ProtocolBuffers-umbrella.h
@@ -0,0 +1,34 @@
+#import <UIKit/UIKit.h>
+
+#import "AbstractMessage.h"
+#import "AbstractMessageBuilder.h"
+#import "Bootstrap.h"
+#import "CodedInputStream.h"
+#import "CodedOutputStream.h"
+#import "ConcreteExtensionField.h"
+#import "Descriptor.pb.h"
+#import "ExtendableMessage.h"
+#import "ExtendableMessageBuilder.h"
+#import "ExtensionField.h"
+#import "ExtensionRegistry.h"
+#import "Field.h"
+#import "ForwardDeclarations.h"
+#import "GeneratedMessage.h"
+#import "GeneratedMessageBuilder.h"
+#import "Message.h"
+#import "MessageBuilder.h"
+#import "MutableExtensionRegistry.h"
+#import "MutableField.h"
+#import "ObjectivecDescriptor.pb.h"
+#import "PBArray.h"
+#import "ProtocolBuffers.h"
+#import "RingBuffer.h"
+#import "TextFormat.h"
+#import "UnknownFieldSet.h"
+#import "UnknownFieldSetBuilder.h"
+#import "Utilities.h"
+#import "WireFormat.h"
+
+FOUNDATION_EXPORT double ProtocolBuffersVersionNumber;
+FOUNDATION_EXPORT const unsigned char ProtocolBuffersVersionString[];
+
